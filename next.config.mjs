7d84512578dc7851a6dad2459d4a7a4ca1e62381
arnab-098 <arnab.chattopadhyay777@gmail.com>
2024-11-15 13:30:17 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverActions: true,
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
